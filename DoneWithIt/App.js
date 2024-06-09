89d82692b5f3f069cd9954a91e83b3e1da744130
Morgan Hondros <morganhondros@gmail.com>
2024-06-09 08:40:40 -0400

    Playing with a button

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("text pressed");
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-      {/* <Image
-        source={require("./assets/icon.png")}
-        style={{ width: 200, height: 200 }}
-      /> */}
-      <Image
-        // blurRadius={1}
-        fadeDuration={1000}
-        source={{ uri: "https://picsum.photos/200/300" }}
-        style={{ width: 200, height: 200 }}
-      />
+      {/* <StatusBar style="auto" /> */}
+      <Button title="Click Me" />
     </SafeAreaView>
   );
 }
